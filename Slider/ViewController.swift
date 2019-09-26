90afac148329c3eae5b2995a61292964aee4ec9d
rikaguri <kirarika7@gmail.com>
2019-09-26 17:32:40 +0900

    Initial Commit

---
+++ Slider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slider
+//
+//  Created by Erika Ura on 2019/09/26.
+//  Copyright © 2019年 Erika Ura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
